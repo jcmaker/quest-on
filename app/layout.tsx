59b519ea9f42ebb1c73eb119e401b67deb6b136f
jcmaker0627 <jcmaker0627@gmail.com>
2026-04-06 21:46:34 +0900

    speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Geist, Geist_Mono, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -64,6 +65,7 @@ export default function RootLayout({
         suppressHydrationWarning={true}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
